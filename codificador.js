7a9c842543e6c8415551e3d7ff7b34b462924a32
Hadassa Moraes <had.moraes@gmail.com>
2022-01-20 13:06:22 -0300

    add codificador.js

---
+++ codificador.js
@@ -0,0 +1,12 @@
+var cifra = document.querySelector('.cifraEscolha')
+var entradasRadiais = document.querySelector('.entradasRadiais');
+
+cifra.addEventListener('submit', function(event){
+    event.preventDefault();
+    console.log('cifra')
+})
+
+/*entradasRadiais.addEventListener('click', function(){
+    entradasRadiais.style.visibility = 'hidden'
+})*/
+
