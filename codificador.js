1d6b907fc426e64ad1ac9b943126a991eaf3df0d
Hadassa Moraes <had.moraes@gmail.com>
2022-01-20 16:24:11 -0300

    adicionei incremento no js

--- codificador.js
+++ codificador.js
@@ -1,7 +1,24 @@
-
+var cifras = document.getElementById("escolhaCifra");
 var codificar = document.querySelector('#codificar');
 var decodificar = document.querySelector('#decodificar');
+var campoCifra = document.querySelector('.cifraEscolha');
+var incremento = document.createElement('input');
+var incrementoLabel = document.createElement('label');
+var lineBreak = document.createElement('br');
+
+incremento.setAttribute('type','text');
+incremento.setAttribute('name','incremento');
+incremento.setAttribute('id', 'mensagem');
+incrementoLabel.setAttribute('for','incremento');
+incrementoLabel.innerText = 'Digite o incremento:';
 
 
+cifras.addEventListener('change', function(){
+    if(cifras.value == 'cesar'){
+        campoCifra.append(incrementoLabel);
+        campoCifra.append(lineBreak);
+        campoCifra.append(incremento);
+    }
+});
 
 
